59afd4bd856748d7e20e9b19ab854382b44ead9b
Huda Abdullah <102191531+hudaabdllah@users.noreply.github.com>
2024-02-12 21:14:23 +0200

    trying

---
+++ script.js
@@ -0,0 +1,19 @@
+
+
+let div = ""
+const container = document.querySelector(".container");
+
+
+for (let i = 0; i < 100; i++) {
+    div = document.createElement("div");
+    div.classList.add("divs");
+    
+    container.appendChild(div);
+}
+
+
+
+
+
+
+
